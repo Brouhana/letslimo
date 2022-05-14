02048e7c688a42b5bf6a628cfa2f2e9b57ef60cc
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2022-05-14 17:46:15 -0500

    prepare user resource

--- app/api/resources/user.py
+++ app/api/resources/user.py
@@ -1,15 +1,22 @@
+from flask import (
+    request,
+    jsonify,
+    current_app as app
+)
 from flask.views import MethodView
+from http import HTTPStatus
 
 
 class UserResource(MethodView):
-    def get(self, user_id):
-        return "Get all users"
+    def get(self, company_id, user_id):
+        args = request.args.get('user_type')
+        return args, HTTPStatus.OK
 
     def post(self):
         return "Create a new user"
 
-    def put(self, user_id):
+    def put(self, company_id, user_id):
         return "Update user"
 
-    def delete(self, user_id):
+    def delete(self, company_id, user_id):
         return "Delete user"
--- app/api/views.py
+++ app/api/views.py
@@ -7,19 +7,16 @@ from app.api.resources.company import CompanyResource
 api_bp = Blueprint('api', __name__, url_prefix='/api/')
 
 user_view_func = UserResource.as_view('users')
-company_view_func = CompanyResource.as_view('companies')
+company_view_func = CompanyResource.as_view('company')
 
-api_bp.add_url_rule('/users',
-                    methods=['GET'],
+api_bp.add_url_rule('/company/<company_id>/users',
+                    methods=['POST', 'GET'],
                     defaults={'user_id': None},
                     view_func=user_view_func)
-api_bp.add_url_rule('/users',
-                    methods=['POST'],
-                    view_func=user_view_func)
-api_bp.add_url_rule('/users/<user_id>',
+api_bp.add_url_rule('/company/<company_id>/users/<user_id>',
                     methods=['GET', 'PUT', 'DELETE'],
                     view_func=user_view_func)
 
-api_bp.add_url_rule('/companies/<company_id>',
+api_bp.add_url_rule('/company/<company_id>',
                     methods=['GET', 'PUT', 'DELETE'],
                     view_func=company_view_func)
