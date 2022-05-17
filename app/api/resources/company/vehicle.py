0c3311edc0fde0a4ba73f480771f57fb5e37711c
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2022-05-16 23:14:27 -0500

    add vehicle resource

---
+++ app/api/resources/company/vehicle.py
@@ -0,0 +1,45 @@
+from flask import (
+    request,
+    jsonify,
+    current_app as app
+)
+from flask.views import MethodView
+from http import HTTPStatus
+
+from app.models.vehicle import Vehicle
+from app.api.schemas.vehicle import VehicleSchema
+from app.middleware.role_required import role_required
+from app.commons.helpers import can_access_company
+
+
+class VehicleResource(MethodView):
+    @role_required('member')
+    def get(self, company_id):
+        if not can_access_company(company_id):
+            return jsonify({'msg': 'You are not authorized to access this company.'}), HTTPStatus.UNAUTHORIZED
+
+        return jsonify('get'), HTTPStatus.OK
+
+    @role_required('admin')
+    def post(self, company_id):
+        if not can_access_company(company_id):
+            return jsonify({'msg': 'You are not authorized to access this company.'}), HTTPStatus.UNAUTHORIZED
+
+        return jsonify('post'), HTTPStatus.OK
+
+    @role_required('admin')
+    def put(self, company_id):
+        if not can_access_company(company_id):
+            return jsonify({'msg': 'You are not authorized to access this company.'}), HTTPStatus.UNAUTHORIZED
+
+        return jsonify('put'), HTTPStatus.OK
+
+    @role_required('owner')
+    def delete(self, company_id):
+        if not can_access_company(company_id):
+            return jsonify({'msg': 'You are not authorized to access this company.'}), HTTPStatus.UNAUTHORIZED
+
+        return jsonify('delete'), HTTPStatus.OK
+
+
+company_schema = CompanySchema()
--- app/api/views.py
+++ app/api/views.py
@@ -2,12 +2,12 @@ from flask import Blueprint
 
 from app.api.resources.company.user import UserResource
 from app.api.resources.company.company import CompanyResource
+from app.api.resources.company.vehicle import VehicleResource
 
 
 api_bp = Blueprint('api', __name__, url_prefix='/api/company/')
 
 user_view_func = UserResource.as_view('users')
-company_view_func = CompanyResource.as_view('company')
 
 api_bp.add_url_rule('/<int:company_id>/users',
                     methods=['GET', 'POST'],
@@ -17,6 +17,18 @@ api_bp.add_url_rule('/<int:company_id>/users/<user_id>',
                     methods=['GET', 'PUT', 'DELETE'],
                     view_func=user_view_func)
 
+company_view_func = CompanyResource.as_view('company')
+
 api_bp.add_url_rule('/<int:company_id>',
                     methods=['GET', 'PUT', 'DELETE'],
                     view_func=company_view_func)
+
+vehicle_view_func = VehicleResource.as_view('vehicle')
+
+api_bp.add_url_rule('/<int:company_id>/vehicles',
+                    methods=['GET', 'POST'],
+                    defaults={'vehicle_id': None},
+                    view_func=vehicle_view_func)
+api_bp.add_url_rule('/<int:company_id>/vehicles/<vehicle_id>',
+                    methods=['GET', 'PUT', 'DELETE'],
+                    view_func=vehicle_view_func)
