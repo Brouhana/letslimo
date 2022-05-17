e238d4c50e50902da8c72883f5a01e72972f86b2
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2022-05-17 15:45:19 -0500

    on second thought, there's no need

--- app/api/schemas/user.py
+++ app/api/schemas/user.py
@@ -1,11 +1,8 @@
 from app import ma
 from app.models.user import User
-from app.api.schemas.company import CompanySchema
 
 
 class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = User
         exclude = ('password',)
-
-    company = ma.Nested(CompanySchema)
