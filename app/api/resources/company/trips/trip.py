fb12e569a4f69407edbc54ab1e0d3fd35622d9c1
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2022-05-21 17:19:45 -0500

    setup trips

---
+++ app/api/resources/company/trips/trip.py
@@ -0,0 +1,47 @@
+from flask import (
+    request,
+    jsonify,
+    current_app as app
+)
+from flask.views import MethodView
+from http import HTTPStatus
+from marshmallow import ValidationError
+
+from app import db
+from app.models.trips import Trip
+from app.api.schemas.trips.trips import TripSchema
+from app.middleware.role_required import role_required
+from app.commons.helpers import can_access_company
+from app.commons.pagination import paginate
+
+
+class TripResource(MethodView):
+    decorators = [role_required('member')]
+
+    def get(self, company_id, vehicle_id):
+        if not can_access_company(company_id):
+            return jsonify({'msg': 'You are not authorized to access this company.'}), HTTPStatus.UNAUTHORIZED
+
+        return ''
+
+    def post(self, company_id, vehicle_id):
+        if not can_access_company(company_id):
+            return {'msg': 'You are not authorized to access this company.'}, HTTPStatus.UNAUTHORIZED
+
+        return ''
+
+    def put(self, company_id, vehicle_id):
+        if not can_access_company(company_id):
+            return {'msg': 'You are not authorized to access this company.'}, HTTPStatus.UNAUTHORIZED
+
+        return ''
+
+    def delete(self, company_id, vehicle_id):
+        if not can_access_company(company_id):
+            return {'msg': 'You are not authorized to access this company.'}, HTTPStatus.UNAUTHORIZED
+
+        return ''
+
+
+trip_schema = TripSchema(partial=True)
+trips_schema = TripSchema(many=True)
--- app/api/views.py
+++ app/api/views.py
@@ -5,6 +5,7 @@ from app.api.resources.company.company import CompanyResource
 from app.api.resources.company.vehicle import VehicleResource
 from app.api.resources.company.contacts.contacts_company import ContactsCompanyResource
 from app.api.resources.company.contacts.contacts_customers import ContactsCustomerResource
+from app.api.resources.company.trips.trip import TripResource
 
 
 api_bp = Blueprint('api', __name__, url_prefix='/api/company/')
@@ -17,6 +18,7 @@ contacts_company_view_func = ContactsCompanyResource.as_view(
     'contacts_company')
 contacts_customer_view_func = ContactsCustomerResource.as_view(
     'contacts_customer')
+trip_view_func = TripResource.as_view('trip')
 
 
 api_bp.add_url_rule('/<int:company_id>',
@@ -58,3 +60,13 @@ api_bp.add_url_rule('/<int:company_id>/contacts/customers',
 api_bp.add_url_rule('/<int:company_id>/contacts/customers/<int:contacts_customer_id>',
                     methods=['GET', 'PUT', 'DELETE'],
                     view_func=contacts_customer_view_func)
+
+
+# /<int:company_id>/trips
+api_bp.add_url_rule('/<int:company_id>/trips',
+                    methods=['GET', 'POST'],
+                    defaults={'trip_id': None},
+                    view_func=trip_view_func)
+api_bp.add_url_rule('/<int:company_id>/trips/<int:trip_id>',
+                    methods=['GET', 'PUT', 'DELETE'],
+                    view_func=trip_view_func)
