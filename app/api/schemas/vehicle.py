ff20f0c5088c7bc905b75bce58b8744a12f93bc4
Brennan Rouhana <8935419+Brouhana@users.noreply.github.com>
2022-06-16 17:52:50 -0500

    changes to model schema vehicle

--- app/api/schemas/vehicle.py
+++ app/api/schemas/vehicle.py
@@ -11,6 +11,7 @@ class VehicleSchema(ma.SQLAlchemyAutoSchema):
 
     company_id = fields.Integer(required=True)
     name = fields.String(required=True)
+    vehicle_type = fields.Raw(required=True)
     pax_capacity = fields.Integer(
         required=True, validate=validate.Range(min=1))
     license_plate_number = fields.String(required=True)
@@ -23,32 +24,7 @@ class VehicleSchema(ma.SQLAlchemyAutoSchema):
         required=False, validate=validate.Length(max=20))
     is_active = fields.Boolean(required=False)
 
-    has_air_conditioning = fields.Boolean(required=False)
-    has_dance_pole = fields.Boolean(required=False)
-    has_luggage_space = fields.Boolean(required=False)
-    has_tables = fields.Boolean(required=False)
-    has_onboard_bathroom = fields.Boolean(required=False)
-    has_onboard_bar = fields.Boolean(required=False)
-    has_refrigerator = fields.Boolean(required=False)
-    has_trash_can = fields.Boolean(required=False)
-    has_ice_chest = fields.Boolean(required=False)
-    has_wheelchair_accessibility = fields.Boolean(required=False)
-
-    has_aux = fields.Boolean(required=False)
-    has_bluetooth = fields.Boolean(required=False)
-    has_dvd_player = fields.Boolean(required=False)
-    has_karaoke = fields.Boolean(required=False)
-    has_usb = fields.Boolean(required=False)
-    has_power_outlets = fields.Boolean(required=False)
-    has_wifi = fields.Boolean(required=False)
-    has_tv = fields.Boolean(required=False)
-    has_gaming_console = fields.Boolean(required=False)
-
-    is_alcohol_allowed = fields.Boolean(required=False)
-    is_smoking_allowed = fields.Boolean(required=False)
-    is_pets_allowed = fields.Boolean(required=False)
-    is_food_allowed = fields.Boolean(required=False)
-    is_children_allowed = fields.Boolean(required=False)
+    features = fields.Boolean(required=False)
 
     min_total_base_rate = fields.Integer(required=False)
     deadhead_rate_per_mile = fields.Integer(required=False)
